110dca3dff0db928b8372739259dba4d8ead77d2
Tom Smith <tomdsmith1@gmail.com>
2025-02-20 20:57:05 +0000

    chore: clean up

--- apps/router/vite.config.ts
+++ apps/router/vite.config.ts
@@ -4,7 +4,6 @@ import svgr from 'vite-plugin-svgr';
 import path from 'path';
 
 export default defineConfig({
-  base: '',
   plugins: [react() as PluginOption[], svgr() as Plugin],
   server: {
     open: true,
