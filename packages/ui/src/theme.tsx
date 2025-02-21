44fe5e5eb4e436c32f99b4be4b4a0a312a66ec40
Tom Smith <tomdsmith1@gmail.com>
2025-02-21 08:16:55 +0000

    chore: clean up

--- packages/ui/src/theme.tsx
+++ packages/ui/src/theme.tsx
@@ -432,7 +432,7 @@ export const Fonts = () => (
   <Global
     styles={[
       css`
-        @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;900&display=swap');
+        @import url('https://fonts.googleapis.com/css2?family=Inter:wght@300,400;600;900&display=swap');
       `,
       {
         body: { fontFamily: 'Inter' },
