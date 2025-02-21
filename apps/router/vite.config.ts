0b12637c9852fbb7279f110edb3ec629e4afd5f5
Tom Smith <tomdsmith1@gmail.com>
2025-02-21 07:56:51 +0000

    chore: update vite config

--- apps/router/vite.config.ts
+++ apps/router/vite.config.ts
@@ -4,6 +4,7 @@ import svgr from 'vite-plugin-svgr';
 import path from 'path';
 
 export default defineConfig({
+  base: '',
   plugins: [react() as PluginOption[], svgr() as Plugin],
   server: {
     open: true,
