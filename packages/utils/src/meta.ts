0e97475ab4b6e6ddcfb67d5fae146fd89eb113f0
Alex Lewin <alexlwn123@gmail.com>
2025-10-30 11:02:34 +0100

    fix: encode/decode hex properly

--- packages/utils/src/meta.ts
+++ packages/utils/src/meta.ts
@@ -8,19 +8,44 @@ export const fieldsToMeta = (fields: MetaFields): MetaConfig => {
   return Object.fromEntries(fields);
 };
 
-export const metaToHex = (meta: MetaConfig): string => {
-  const str = JSON.stringify(meta);
-  let hex = '';
-  for (let i = 0; i < str.length; i++) {
-    hex += `${str.charCodeAt(i).toString(16)}`;
+/** https://stackoverflow.com/a/57391629 */
+const bytesToHex = (bytes: Uint8Array): string => {
+  return Array.from(bytes, (byte) => byte.toString(16).padStart(2, '0')).join(
+    ''
+  );
+};
+
+/** https://stackoverflow.com/a/57391629 */
+const hexToBytes = (hex: string): Uint8Array => {
+  const bytes = new Uint8Array(hex.length / 2);
+  for (let i = 0; i < bytes.length; i++) {
+    bytes[i] = parseInt(hex.slice(i * 2, i * 2 + 2), 16);
   }
-  return hex;
+  return bytes;
+};
+
+/** https://stackoverflow.com/a/57391629 */
+const stringToUTF8Bytes = (string: string): Uint8Array => {
+  return new TextEncoder().encode(string);
+};
+
+export const metaToHex = (meta: MetaConfig): string => {
+  try {
+    const str = JSON.stringify(meta);
+    const bytes = stringToUTF8Bytes(str);
+    const hex = bytesToHex(bytes);
+    return hex;
+  } catch (error) {
+    throw new Error('Invalid meta object. Failed to encode to hex.');
+  }
 };
 
 export const hexToMeta = (hex: string): MetaConfig => {
-  let str = '';
-  for (let i = 0; i < hex.length; i += 2) {
-    str += String.fromCharCode(Number.parseInt(hex.substr(i, 2), 16));
+  try {
+    const bytes = hexToBytes(hex);
+    const str = new TextDecoder().decode(bytes);
+    return JSON.parse(str);
+  } catch (error) {
+    throw new Error('Invalid hex string. Failed to decode to meta object.');
   }
-  return JSON.parse(str);
 };
